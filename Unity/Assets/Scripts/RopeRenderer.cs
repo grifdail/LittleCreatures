1c63c21b95402331b90f3389386121ea241e663c
KeumarSIG <m.bonte@rubika-edu.com>
2016-11-12 11:41:37 +0100

    New physics system

--- Unity/Assets/Scripts/RopeRenderer.cs
+++ Unity/Assets/Scripts/RopeRenderer.cs
@@ -1,23 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class RopeRenderer : MonoBehaviour {
 
     private LineRenderer _renderer;
-    public Transform target;
+    public Transform[] targets;
 
-	// Use this for initialization
-	void Start () {
-        if (target == null)
-        {
-            //target = GetComponent<Joint>().connectedBody.transform;
-        }
-        
+	void Start ()
+    {      
         _renderer = GetComponent<LineRenderer>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        for (int i = 0; i < targets.Length - 1; i++)
+        {
+            _renderer.SetPosition(i, targets[i + 1].position);
+        }
+
         //_renderer.SetPosition(0, transform.position);
         //_renderer.SetPosition(1, target.position);
 	}
